01ebc8284c84fef5fda5fff82e8087a2cb48ea9f
jungmin <kimjm0825@gmail.com>
2023-03-18 02:49:17 +0900

    MyPageContract 작성

--- CatWork/app/src/main/java/com/example/catwork/presentation/mypage/MyPageContract.kt
+++ CatWork/app/src/main/java/com/example/catwork/presentation/mypage/MyPageContract.kt
@@ -1,4 +1,24 @@
 package com.example.catwork.presentation.mypage
 
+import com.example.catwork.data.entity.ToDoEntity
+import com.example.catwork.presentation.BasePresenter
+import com.example.catwork.presentation.BaseView
+
 interface MyPageContract {
+
+    interface View : BaseView<Presenter> {
+
+        fun showLoadingIndicator()
+
+        fun hideLoadingIndicator()
+
+        fun showErrorDescription(message: String)
+
+        fun showCalendarRecord()
+    }
+
+    interface Presenter : BasePresenter {
+
+        fun fetchCalendarRecord(date: String)
+    }
 }
