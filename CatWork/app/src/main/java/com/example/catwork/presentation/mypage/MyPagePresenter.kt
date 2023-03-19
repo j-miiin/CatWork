5d74a23fc13d3337e0527a9dc1466e6423f0e8d5
jungmin <kimjm0825@gmail.com>
2023-03-19 23:57:34 +0900

    MyPagePresenter 작성

--- CatWork/app/src/main/java/com/example/catwork/presentation/mypage/MyPagePresenter.kt
+++ CatWork/app/src/main/java/com/example/catwork/presentation/mypage/MyPagePresenter.kt
@@ -1,4 +1,19 @@
 package com.example.catwork.presentation.mypage
 
-class MyPagePresenter {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.MainScope
+
+class MyPagePresenter: MyPageContract.Presenter {
+
+    override val scope: CoroutineScope = MainScope()
+
+    override fun onViewCreated() {
+        
+    }
+
+    override fun onDestroyView() {}
+
+    override fun fetchCalendarRecord(date: String) {
+
+    }
 }
